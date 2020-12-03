e2d66e5c636172348383069ef679a323532ea47d
Tal Zana <talzana55@gmail.com>
2020-12-03 20:41:19 +0100

    Look Jmatike

--- app/Http/Controllers/ControllerConnexionUser.php
+++ app/Http/Controllers/ControllerConnexionUser.php
@@ -14,24 +14,41 @@ class ControllerConnexionUser extends Controller
     public function formulaire(){
         return view('connexions/connexionUser');
     }
-
-    public function traitement (Request $request){
-
-        request()->validate([
-            'mail' => ['required'],
-            'password' => ['required'],
-        ]);
-//        dd(request());
-        //$resultat= Auth::attempt($request->only['email', 'password']);
-               $resultat= Auth::attempt($request->only('email', 'password'));
-//        $resultat= auth()->attempt([
-//
-//            'email'=> request('email'),
-//            'password'=> Hash::make(request('password')),
-
-       // ]);
-       var_dump($resultat);
-        return'traitement de la connexion';
+    public function traitement (Request $request)
+    {
+        dd($request-email);
+        $user = User::whereEmail($request->post('email'));
+        dd(Hash::check($request->post('password'), $user->password));
     }
+//    public function traitement (Request $request){
+//       // dd($request->input('email'));
+//        // $request->dd(['email, 'password']);
+//        request()->validate([
+//            'mail' => ['required'],
+//            'password' => ['required'],
+//        ]);
+////        dd(request());
+//        //$resultat= Auth::attempt($request->only['email', 'password']);
+//               $resultat= Auth::attempt($request->only('email', 'password'));
+////        $resultat= auth()->attempt([
+////
+////            'email'=> request('email'),
+////            'password'=> Hash::make(request('password')),
+//
+//       // ]);
+//       var_dump($resultat);
+//        return'traitement de la connexion';
+//    }
+//    public function traitement (Request $request) {
+//        $request->validate([
+//            'mail' => ['required', 'email'],
+//            'password' => ['required'],
+//        ]);
+//
+//        $resultat = Auth::attempt($request->only('email', 'password'));
+//
+//        dd($resultat);
+//        return 'traitement de la connexion';
+//    }
 
 }
