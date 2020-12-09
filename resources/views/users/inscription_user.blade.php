554880d5cd35da0b706f61221534d5478f597712
tzlan <talzana55@gmail.com>
2020-12-10 00:07:54 +0100

    Mot de passe

--- resources/views/users/inscription_user.blade.php
+++ resources/views/users/inscription_user.blade.php
@@ -42,7 +42,7 @@
 <div class="container p-3 my-3 border ">
 
     <h1>Formulaire d'inscription  </h1>
-    <p>Faites attention</p>
+    <p>Faites attention a retenir votre mot de passe</p>
     <br>
     <br>
     <br>
